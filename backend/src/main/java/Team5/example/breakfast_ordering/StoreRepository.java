a182f2a296d81cf5daf0b598f0390b4ac0731608
MOMIJI05 <sandykang05@gmail.com>
2025-11-22 13:54:40 +0800

    refactor: 將 Store 的 model, controller, repository更改位置並更新欄位

--- backend/src/main/java/Team5/example/breakfast_ordering/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package Team5.example.breakfast_ordering;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoreRepository extends MongoRepository<Store, String> {
-}
