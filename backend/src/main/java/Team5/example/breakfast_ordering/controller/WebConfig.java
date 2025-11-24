7039f9edfc2156943776a828972b6c3f04b07714
MOMIJI05 <sandykang05@gmail.com>
2025-11-24 23:17:05 +0800

    feat: 新增 WebConfig.java 讓後端不會擋住前端的請求

---
+++ backend/src/main/java/Team5/example/breakfast_ordering/controller/WebConfig.java
@@ -0,0 +1,18 @@
+package Team5.example.breakfast_ordering.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:5173") // 允許的前端網址
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
