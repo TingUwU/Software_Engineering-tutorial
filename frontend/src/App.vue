d8f5c6035f4aa1a30fa1fb379226db21a647c324
yinhsuan0218-cmyk <yinhsuan0218@gmail.com>
2025-11-25 22:50:53 +0800

    更新專案名稱

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,65 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>|
-    <router-link to="/login">Login</router-link>|
-    <router-link to="/cart">Cart</router-link>|
-    <router-link to="/favorite">Favorite</router-link>|
-  </nav>
-  <router-view/>
+    <div id="app">
+        <nav>
+            <router-link to="/">
+                <img class="avatar"
+                     :src="require('@/assets/logo.png')"
+                     alt="logo">
+                早一點
+            </router-link>
+        </nav>
+
+        <!-- HomeView.vue 會在這裡渲染 -->
+        <router-view />
+    </div>
 </template>
 
+<script>
+    export default {
+        data() {
+            return {
+                customer: {
+                    photo: null
+                }
+            };
+        }
+    };
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+    }
 
-nav {
-  padding: 30px;
-}
+    nav {
+        padding: 30px;
+    }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+        nav a {
+            font-weight: bold;
+            color: #000; /* 黑色字 */
+            text-decoration: none; /* 去掉底線 */
+        }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+            /* 當前頁面文字顯示藍色 */
+            nav a.router-link-exact-active {
+                color: #0069D9; /* 藍色 */
+            }
+
+            nav a:hover {
+                color: #0069D9;
+            }
+
+
+    .avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        vertical-align: middle;
+        margin-right: 10px;
+    }
 </style>
+
