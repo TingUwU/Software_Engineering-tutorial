f0ad352dda37f63c3762d7103895429c191134dc
MOMIJI05 <sandykang05@gmail.com>
2026-01-03 01:38:30 +0800

    fix: remove allowedOriginPatterns

--- backend/src/main/java/Team5/example/breakfast_ordering/controller/WebConfig.java
+++ backend/src/main/java/Team5/example/breakfast_ordering/controller/WebConfig.java
@@ -11,7 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://breakfast-frontend.onrender.com") // 允許的前端網址
-                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
