33abcfe175c4d748a6f37f41ff01070a5be4d69c
MOMIJI05 <sandykang05@gmail.com>
2026-01-03 15:53:43 +0800

    fix: 修改 vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,13 +2,26 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  transpileDependencies: true,
+
   devServer: {
     proxy: {
       // 如果你的 API 請求路徑是以 /api 開頭，例如 /api/users, /api/products
       // 代理就會捕捉到這些請求
       '/api': {
-        target: 'http://localhost:8080', // 這裡指向你 Spring Boot 後端的地址
+        target: 'http://localhost:8080', // 這裡指向 Spring Boot 後端的地址
         changeOrigin: true, // 允許跨域
+      }
+    }
+  },
+
+  // 新增 Webpack 設定
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "net": false,
+        "tls": false,
+        "fs": false
       }
     }
   }
